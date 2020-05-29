016ce4e4c206c0692cae46c9b12dc9769d4fcffc
arijitnaha <arijitnaha.kolkata@yahoo.com>
2020-05-30 04:11:21 +0530

    initialise 2

---
+++ test 2.py
@@ -0,0 +1,15 @@
+import sys
+
+# import requests
+# import regex
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greetings = "Hello, {}".format(who_to_greet)
+    return greetings
+
+
+print(greet("Blue"))
+print(greet("World"))
